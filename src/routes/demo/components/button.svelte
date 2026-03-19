f3ad1ecf7efb5b962044b1b8d7a7f084e7ec35dd
Alan Panayotov <alanpanayotov@gmail.com>
2026-03-19 15:36:30 +0000

    feat (demo): add disabled buttons

--- src/routes/demo/components/button.svelte
+++ src/routes/demo/components/button.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
 	import { Button, buttonVariants } from '$ui/button';
 	import * as Card from '$ui/card';
+
+	let variants = Object.keys(
+		buttonVariants.variants.variant
+	) as (keyof typeof buttonVariants.variants.variant)[];
+	let sizes = Object.keys(
+		buttonVariants.variants.size
+	) as (keyof typeof buttonVariants.variants.size)[];
 </script>
 
 <Card.Root id="button">
@@ -8,15 +15,22 @@
 		<Card.Title>Button</Card.Title>
 	</Card.Header>
 	<Card.Content class="grid grid-cols-[repeat(auto-fit,minmax(35ch,1fr))] gap-2">
-		{#each Object.keys(buttonVariants.variants.variant) as (keyof typeof buttonVariants.variants.variant)[] as variant}
+		{#each variants as variant}
 			<Card.Root>
 				<Card.Header>
 					<Card.Title>{variant}</Card.Title>
 				</Card.Header>
-				<Card.Content class="flex items-center justify-center gap-2">
-					{#each Object.keys(buttonVariants.variants.size) as (keyof typeof buttonVariants.variants.size)[] as size}
-						<Button {variant} {size}>{size}</Button>
-					{/each}
+				<Card.Content class="flex flex-col gap-1">
+					<div class="flex items-center justify-center gap-2">
+						{#each sizes as size}
+							<Button {variant} {size}>{size}</Button>
+						{/each}
+					</div>
+					<div class="flex items-center justify-center gap-2">
+						{#each sizes as size}
+							<Button {variant} {size} disabled>{size}</Button>
+						{/each}
+					</div>
 				</Card.Content>
 			</Card.Root>
 		{/each}
