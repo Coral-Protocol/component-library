618790355d86cfdfdb48dd7452f01930de84f56b
Alan Panayotov <alanpanayotov@gmail.com>
2026-03-26 10:49:44 +0000

    feat(demo): add context menu

--- src/routes/(library)/+page.svelte
+++ src/routes/(library)/+page.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 	import Button from './components/button.svelte';
 	import Checkbox from './components/checkbox.svelte';
+	import ContextMenu from './components/context-menu.svelte';
 	import Combobox from './components/custom/combobox.svelte';
 </script>
 
 <main class="mx-[5%] flex flex-col gap-8 py-8 lg:mx-auto lg:w-4/5">
 	<Button />
 	<Checkbox />
+	<ContextMenu />
 	<Combobox />
 </main>
