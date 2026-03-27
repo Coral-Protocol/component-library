ba36618586068e946bb513aa3bb5fa54b898f348
Alan Panayotov <alanpanayotov@gmail.com>
2026-03-27 15:33:28 +0000

    feat(demo): add btn text override, size visibility toggles & icon size special-case (#16)

--- src/routes/(library)/components/button.svelte
+++ src/routes/(library)/components/button.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
 	import { Button, buttonVariants } from '$ui/button';
+
+	import { Checkbox } from '$ui/checkbox';
+	import { Label } from '$ui/label';
+	import { Input } from '$ui/input';
+
 	import * as Card from '$ui/card';
+
+	import IconArchive from 'phosphor-icons-svelte/IconArchiveRegular.svelte';
+	import { Separator } from '@coral-os/component-library/components/ui/separator';
 
 	let variants = Object.keys(
 		buttonVariants.variants.variant
@@ -8,6 +16,9 @@
 	let sizes = Object.keys(
 		buttonVariants.variants.size
 	) as (keyof typeof buttonVariants.variants.size)[];
+
+	let sizeVisibility = $state(Object.fromEntries(sizes.map((s) => [s, true])));
+	let textOverride = $state('');
 </script>
 
 <Card.Root id="button">
@@ -15,20 +26,51 @@
 		<Card.Title>Button</Card.Title>
 	</Card.Header>
 	<Card.Content class="grid grid-cols-[repeat(auto-fit,minmax(35ch,1fr))] gap-2">
+		<Card.Root class="">
+			<Card.Content class="flex flex-col gap-2">
+				<h2 class="font-sans font-bold">Visibility</h2>
+				<section class="flex gap-2">
+					{#each sizes as size}
+						<Label>
+							<Checkbox bind:checked={sizeVisibility[size]} />
+							{size}
+						</Label>
+					{/each}
+				</section>
+				<Separator class="my-2" />
+				<Input bind:value={textOverride} placeholder="Text Override" />
+			</Card.Content>
+		</Card.Root>
 		{#each variants as variant}
 			<Card.Root>
 				<Card.Header>
 					<Card.Title>{variant}</Card.Title>
 				</Card.Header>
 				<Card.Content class="flex flex-col gap-1">
-					<div class="flex items-center justify-center gap-2">
+					<div class="flex flex-wrap items-center justify-center gap-2">
 						{#each sizes as size}
-							<Button {variant} {size}>{size}</Button>
+							{#if sizeVisibility[size]}
+								<Button {variant} {size}
+									>{#if size === 'icon'}
+										<IconArchive />
+									{:else}
+										{textOverride || size}
+									{/if}</Button
+								>
+							{/if}
 						{/each}
 					</div>
-					<div class="flex items-center justify-center gap-2">
+					<div class="flex flex-wrap items-center justify-center gap-2">
 						{#each sizes as size}
-							<Button {variant} {size} disabled>{size}</Button>
+							{#if sizeVisibility[size]}
+								<Button {variant} {size} disabled
+									>{#if size === 'icon'}
+										<IconArchive />
+									{:else}
+										{textOverride || size}
+									{/if}</Button
+								>
+							{/if}
 						{/each}
 					</div>
 				</Card.Content>
