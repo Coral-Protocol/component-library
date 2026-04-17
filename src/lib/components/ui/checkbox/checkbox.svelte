7c759207c519632f670a343a6e9d5a9e5be6986b
Luka Forder <lukaforder@gmail.com>
2026-04-17 16:16:16 +0100

    fix: checkbox

--- src/lib/components/ui/checkbox/checkbox.svelte
+++ src/lib/components/ui/checkbox/checkbox.svelte
@@ -17,7 +17,7 @@
 	bind:ref
 	data-slot="checkbox"
 	class={cn(
-		'peer flex size-4.5 shrink-0 items-center justify-center border border-border shadow-xs transition-shadow outline-none focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 data-[state=checked]:border-border data-[state=checked]:bg-accent data-[state=checked]:text-primary-foreground dark:bg-border/30 dark:aria-invalid:ring-destructive/40 dark:data-[state=checked]:bg-accent',
+		'peer relative flex size-4 shrink-0 items-center justify-center  border border-input shadow-xs transition-shadow outline-none group-has-disabled/field:opacity-50 after:absolute after:-inset-x-3 after:-inset-y-2 focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 aria-invalid:aria-checked:border-primary dark:bg-input/30 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40 data-checked:border-primary data-checked:bg-primary data-checked:text-primary-foreground dark:data-checked:bg-primary',
 		className
 	)}
 	bind:checked
